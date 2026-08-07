dc0fd1fb4180da7722ede5e76f6a7104ce76f62a
tobiasstoll-1 <t.stoll.1@web.de>
2026-08-07 16:40:30 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v9";
+const APP_VERSION = "v10";
 const STATIC_CACHE = `zehnkampfrechner-static-${APP_VERSION}`;
 const RUNTIME_CACHE = `zehnkampfrechner-runtime-${APP_VERSION}`;
 
